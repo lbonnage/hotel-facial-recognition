265892cf81865bac0e7b10061887e8dd299e5de0
Liam Bonnage <lbonnage@gmail.com>
2020-03-22 20:16:12 -0500

    first commit

---
+++ hotel_camera.py
@@ -0,0 +1,4 @@
+import hotel_facial_recognition
+
+def detect_face():
+	hotel_facial_recognition.detect_guest()
---
+++ hotel_facial_recognition.py
@@ -0,0 +1,18 @@
+def add_guest():
+	pass
+
+
+def detect_guest():
+	pass
+
+
+def remove_guest():
+	pass
+
+
+def valid_guest_detected():
+	pass
+
+
+def invalid_guest_detected():
+	pass
